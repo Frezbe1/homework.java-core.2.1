a244ef52a22cce67d897e2e4fb313e67374ebaba
Шалагинов Виталий <v.a.sh@mail.ru>
2022-08-27 19:37:39 +0700

    java-core.2.1

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> newList = new ArrayList<>();
+
+        for (int i : intList) {
+            if (i <= 0) {
+                continue;
+            }
+            if (i % 2 != 0) {
+                continue;
+            }
+            newList.add(i);
+        }
+        Collections.sort(newList);
+
+        for (int i : newList) System.out.println(i);
+    }
+}
